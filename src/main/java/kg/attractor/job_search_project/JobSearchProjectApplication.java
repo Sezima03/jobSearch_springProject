f19451efc5867abd60150983ac9722ca7f1e2544
Sezima.n <sezimanurlanovna03@gmail.com>
2025-03-12 21:52:42 +0600

    .

---
+++ src/main/java/kg/attractor/job_search_project/JobSearchProjectApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.job_search_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobSearchProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/attractor/job_search_project/JobSearchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.job_search_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
