34c49dc26b280686ec369e6ed704024f8aa9da10
Sezima.n <sezimanurlanovna03@gmail.com>
2025-03-12 21:46:13 +0600

    сreated a Message class

---
+++ src/main/java/kg/attractor/job_search_project/model/Message.java
@@ -0,0 +1,10 @@
+package kg.attractor.job_search_project.model;
+
+import java.sql.Timestamp;
+
+public class Message {
+    private int id;
+    private int responded_applicants;
+    private String content;
+    private Timestamp timestamp;
+}
