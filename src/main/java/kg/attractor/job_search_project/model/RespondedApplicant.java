825b64f833c70bf5d25b8cd339a38299690f75e4
Sezima.n <sezimanurlanovna03@gmail.com>
2025-03-12 21:45:49 +0600

    сreated a RespondedApplicant class

---
+++ src/main/java/kg/attractor/job_search_project/model/RespondedApplicant.java
@@ -0,0 +1,8 @@
+package kg.attractor.job_search_project.model;
+
+public class RespondedApplicant {
+    private int id;
+    private int resume_id;
+    private int vacancy_id;
+    private boolean confirmation;
+}
