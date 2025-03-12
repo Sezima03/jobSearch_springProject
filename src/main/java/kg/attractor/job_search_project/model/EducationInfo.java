25b015723aff569b610643a64b1769ab6b45c418
Sezima.n <sezimanurlanovna03@gmail.com>
2025-03-12 21:42:12 +0600

    сreated a EducationInfo class

---
+++ src/main/java/kg/attractor/job_search_project/model/EducationInfo.java
@@ -0,0 +1,13 @@
+package kg.attractor.job_search_project.model;
+
+import java.time.LocalDate;
+
+public class EducationInfo {
+    private int id;
+    private int resume_id;
+    private String institution;
+    private String program;
+    private LocalDate start_date;
+    private LocalDate end_date;
+    private String degree;
+}
