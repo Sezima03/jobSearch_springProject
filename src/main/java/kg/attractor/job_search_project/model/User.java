bd30731ffc465f1a749f01e71f55f3099487e88d
Sezima.n <sezimanurlanovna03@gmail.com>
2025-03-12 21:43:01 +0600

    сreated a User class

--- src/main/java/kg/attractor/job_search_project/model/User.java
+++ src/main/java/kg/attractor/job_search_project/model/User.java
@@ -1,4 +1,13 @@
 package kg.attractor.job_search_project.model;
 
 public class User {
+    private int id;
+    private String name;
+    private String surname;
+    private int age;
+    private String email;
+    private String password;
+    private String phone_number;
+    private String avatar;
+    private String account_type;
 }
