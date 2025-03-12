c04ba825b5c150dfd399da942cd1f2942a0fcab4
Sezima.n <sezimanurlanovna03@gmail.com>
2025-03-12 21:43:21 +0600

    сreated a WorkExperienceInfo class

---
+++ src/main/java/kg/attractor/job_search_project/model/WorkExperienceInfo.java
@@ -0,0 +1,10 @@
+package kg.attractor.job_search_project.model;
+
+public class WorkExperienceInfo {
+    private int id;
+    private int resume_id;
+    private int years;
+    private String company_name;
+    private String position;
+    private String responsibilities;
+}
