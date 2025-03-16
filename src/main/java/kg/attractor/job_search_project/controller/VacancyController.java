f0abb4b3099ecc7e965d03a68e3429955c656e6a
Sezima.n <sezimanurlanovna03@gmail.com>
2025-03-16 13:01:15 +0600

    Created controllers for employers

--- src/main/java/kg/attractor/job_search_project/controller/VacancyController.java
+++ src/main/java/kg/attractor/job_search_project/controller/VacancyController.java
@@ -1,32 +1,60 @@
 package kg.attractor.job_search_project.controller;
 
-import kg.attractor.job_search_project.dto.VacancyDto;
+import kg.attractor.job_search_project.model.Vacancy;
+import kg.attractor.job_search_project.service.VacancyServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("vacancy")
 @RequiredArgsConstructor
 public class VacancyController {
+    private VacancyServiceImpl vacancyServiceImp;
 
     @PostMapping
-    public HttpStatus createVacancy(@RequestBody VacancyDto vacancyDto) {
-        //TODO logics
-        return HttpStatus.CREATED;
+    public ResponseEntity<Vacancy> createVacancy(@RequestBody Vacancy vacancy) {
+        Vacancy createVacancy=vacancyServiceImp.createdVacancy(vacancy);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createVacancy);
     }
 
-    @PutMapping("{vacancyId}")
-    public ResponseEntity<VacancyDto> updateVacancy(@PathVariable("vacancyId") String id, @RequestBody VacancyDto vacancyDto) {
-        //TODO logic for updating a vacancy
-        return new ResponseEntity<>(vacancyDto, HttpStatus.OK);
+    @PutMapping("update/{vacancyId}")
+    public ResponseEntity<Vacancy> updateVacancy(@PathVariable("vacancyId") String id, @RequestBody Vacancy vacancy) {
+        Vacancy update=vacancyServiceImp.updateVacancy(id, vacancy);
+        return ResponseEntity.status(HttpStatus.OK).body(update);
 
     }
 
-    @DeleteMapping("{vacancyId}")
-    public ResponseEntity<VacancyDto> deleteVacancy(@PathVariable("vacancyId") String id) {
+    @DeleteMapping("delete/{vacancyId}")
+    public ResponseEntity<Void> deleteVacancy(@PathVariable("vacancyId") String id) {
+        vacancyServiceImp.deleteVacancy(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("searchresume")
+    public ResponseEntity<Void> allResume() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("category")
+    public ResponseEntity<List<Vacancy>> vacancyByCategory(@PathVariable("category") String category) {
+        List<Vacancy> vacancyCategory=vacancyServiceImp.getVacancyByCategory(category);
+        return ResponseEntity.ok(vacancyCategory);
+    }
+
+    @GetMapping("responded")
+    public ResponseEntity<List<Vacancy>> searchRespondedToVacancy(){
+        List<Vacancy> search = vacancyServiceImp.getRespondedToVacancy();
+        return ResponseEntity.ok(search);
+    }
+
+    @GetMapping("applicant")
+    public ResponseEntity <List<Vacancy>> searchForApplicant(@RequestParam String name){
+        List<Vacancy> search = vacancyServiceImp.getSearchApplicant(name);
+        return ResponseEntity.ok(search);
     }
 
 }
