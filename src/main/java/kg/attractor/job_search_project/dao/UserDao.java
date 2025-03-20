9fc3173fb3cdfe3541345066c3e96af2584e4ad7
Sezima.n <sezimanurlanovna03@gmail.com>
2025-03-20 22:49:28 +0600

    Added methods for query handling

--- src/main/java/kg/attractor/job_search_project/dao/UserDao.java
+++ src/main/java/kg/attractor/job_search_project/dao/UserDao.java
@@ -1,8 +1,10 @@
 package kg.attractor.job_search_project.dao;
 
 import kg.attractor.job_search_project.dto.UserDto;
+import kg.attractor.job_search_project.model.Resume;
 import kg.attractor.job_search_project.model.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -24,5 +26,32 @@ public class UserDao {
     public List<User> getSearchByNumber(String phone_number) {
         String sql = "select * from users where phone_number like ?";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class), phone_number);
+    }
+
+    public List<User> getSearchByEmail(String email) {
+        String sql = "select * from users where email like ?";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class), email);
+    }
+
+    public User isEmailTaken(String email) {
+        String sql = "select * from users where email = ? LIMIT 1";
+        try {
+            return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(User.class), email);
+        }catch (EmptyResultDataAccessException e){
+            return null;
+        }
+    }
+
+    public void saveUser(User user){
+        String sql="insert into users(name, surname, age, email, password, phone_number, avatar, account_type) values(?,?,?,?,?,?,?,?)";
+
+        jdbcTemplate.update(sql, user.getName(), user.getSurname(), user.getAge(), user.getEmail(), user.getPassword(), user.getPhoneNumber(), user.getAvatar(), user.getAccountType());
+    }
+
+    public List<Resume> searchByCategory(String category){
+        String sql = "SELECT r.* FROM resume r " +
+                "JOIN categories c ON r.category_id = c.id " +
+                "WHERE c.name = ?";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Resume.class), category);
     }
 }
