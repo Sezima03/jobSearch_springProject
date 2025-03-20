98425a2d02951c0499e0c8c4cb243666cb624635
Sezima.n <sezimanurlanovna03@gmail.com>
2025-03-20 22:41:25 +0600

    changed and added script and data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,6 +11,54 @@ create table if not exists users(
 
     );
 
+create table if not exists categories(
+                                         id int auto_increment primary key,
+                                         parent_id int,
+                                         name varchar(50)
+    );
+
+create table if not exists resume(
+                                     id int auto_increment primary key,
+                                     applicant_id int,
+                                     name varchar(50),
+    category_id int,
+    salary double,
+    is_active boolean,
+    created_date timestamp default current_timestamp,
+    update_time timestamp default current_timestamp on update current_timestamp,
+    foreign key(category_id) references categories(id) on delete restrict on update cascade
+
+    );
+
+create table if not exists vacancy(
+                                      id int auto_increment primary key,
+                                      name varchar(50),
+    description varchar(500),
+    category_id int,
+    salary double,
+    exp_from int,
+    exp_to int,
+    is_active boolean,
+    author_id int,
+    created_date timestamp,
+    update_time timestamp,
+    foreign key(category_id) references categories(id) on delete restrict on update cascade
+    );
+
+create table if not exists responded_applicant(
+                                                  id int auto_increment primary key,
+                                                  resume_id int,
+                                                  vacancy_id int,
+                                                  confirmation boolean,
+
+                                                  foreign key(resume_id) references resume(id)
+    on delete restrict on update cascade,
+
+    foreign key(vacancy_id) references vacancy(id)
+    on delete restrict on update cascade
+    );
+
+
 insert into users(name,
                   surname,
                   age,
@@ -22,19 +70,6 @@ insert into users(name,
 values ('applicantTom', 'maksimov', 20, 'tommaksimov@gmail.com', 'qw12', '996707123456', 'data/images/ava.jpg', 'applicant'),
        ('employerAlex', 'ashimov', 19, 'alexashimov@gmail.com', 'alex12', '996700231546', 'data/images/ava.jpg', 'employer');
 
-
-create table if not exists resume(
-                                     id int auto_increment primary key,
-                                     applicant_id int,
-                                     name varchar(50),
-    category_id int,
-    salary double,
-    is_active boolean,
-    created_date timestamp default current_timestamp,
-    update_time timestamp default current_timestamp on update current_timestamp
-
-    );
-
 insert into resume(
     applicant_id,
     name,
@@ -43,23 +78,11 @@ insert into resume(
     is_active,
     created_date,
     update_time)
-values (1, 'java junior', 1, 60000, true, now(), now()),
-       (1, 'senior backend developer', 2, 200000, true, now(), now());
+values (1, 'java junior', 2 , 60000, true, now(), now()),
+       (1, 'senior backend developer', 2, 200000, true, now(), now()),
+       (1, 'frontend developer', 1, 90000, true,now(), now());
 
 
-create table if not exists vacancy(
-    id int auto_increment primary key,
-    name varchar(50),
-    description varchar(500),
-    category_id int,
-    salary double,
-    exp_from int,
-    exp_to int,
-    is_active boolean,
-    author_id int,
-    created_date timestamp,
-    update_time timestamp
-    );
 insert into vacancy(
     name,
     description,
@@ -70,24 +93,17 @@ insert into vacancy(
     author_id,
     created_date,
     update_time)
-values ('Frontend Developer', 'Join our frontend team to develop user interfaces using React.js and modern JavaScript frameworks', 100000, 1, 3, true, 2, now(), now()),
-       ('Senior Backend Developer', 'We are looking for a Senior Backend Developer', 180000, 3, 6, true, 2, now(), now());
+values ('Frontend Developer', 'Join our frontend team to develop user interfaces using React.js and modern JavaScript frameworks',100000, 1, 3, true, 2, now(), now()),
+       ('Senior Backend Developer', 'We are looking for a Senior Backend Developer',180000, 3, 6, true, 2, now(), now());
 
-
-create table if not exists responded_applicant(
-                                                  id int auto_increment primary key,
-                                                  resume_id int,
-                                                   vacancy_id int,
-                                                  confirmation boolean,
-
-                                                  foreign key(resume_id) references resume(id)
-    on delete restrict on update cascade,
-
-    foreign key(vacancy_id) references vacancy(id)
-    on delete restrict on update cascade
-    );
 
 insert into responded_applicant(resume_id,
                                 vacancy_id,
                                 confirmation)
-values (1, 2, true)
+values (1, 2, true);
+
+insert into categories(parent_id, name) values
+                                            (1,'js'),
+                                            (2, 'java'),
+                                            (3, 'python'),
+                                            (4, 'project manager')
