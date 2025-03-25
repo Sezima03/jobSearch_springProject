136ad1d7c4acc87bff6fcabf0efc478f06c56635
Sezima.n <sezimanurlanovna03@gmail.com>
2025-03-25 11:27:12 +0600

    Removed obsolete controllers and services

--- src/main/java/kg/attractor/job_search_project/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package kg.attractor.job_search_project.service;
-
-import kg.attractor.job_search_project.dto.UserDto;
-import kg.attractor.job_search_project.model.User;
-
-public interface AuthService {
-    String registerUser(User user);
-}
