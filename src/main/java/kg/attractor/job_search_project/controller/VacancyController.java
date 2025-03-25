272bce2887853525b41c34c7fd882345c67095a9
Sezima.n <sezimanurlanovna03@gmail.com>
2025-03-25 11:15:22 +0600

    added cotrollor

--- src/main/java/kg/attractor/job_search_project/controller/VacancyController.java
+++ src/main/java/kg/attractor/job_search_project/controller/VacancyController.java
@@ -1,7 +1,7 @@
 package kg.attractor.job_search_project.controller;
+import kg.attractor.job_search_project.dto.RespondedApplicantDto;
 import kg.attractor.job_search_project.dto.VacancyDto;
 import kg.attractor.job_search_project.service.VacancyService;
-import kg.attractor.job_search_project.service.impl.VacancyServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,27 +11,26 @@ import java.util.List;
 @RestController
 @RequestMapping("vacancy")
 @RequiredArgsConstructor
-public class EmployerController {
+public class VacancyController {
     private final VacancyService vacancyService;
 
-    //TODO работает
     @GetMapping
     public List<VacancyDto> vacancy(){
         return vacancyService.getVacancy();
     }
-//TODO работвет
+
     @PostMapping("add")
     public HttpStatus addVacancy(@RequestBody VacancyDto vacancyDto){
         vacancyService.createdVacancy(vacancyDto);
         return HttpStatus.OK;
     }
-//TODO yes
+
     @PutMapping("update/{vacancyId}")
     public HttpStatus updateVacancy(@RequestBody VacancyDto vacancyDto, @PathVariable Long vacancyId){
         vacancyService.updateVacancy(vacancyDto, vacancyId);
         return HttpStatus.OK;
     }
-//TODO yes
+
     @DeleteMapping("delete/{vacancyId}")
     public HttpStatus deleteVacancy(@PathVariable("vacancyId") Long id) {
         vacancyService.deleteVacancy(id);
@@ -41,5 +40,16 @@ public class EmployerController {
     @GetMapping("category/{id}")
     public List<VacancyDto> getVacancyByCategoryId(@PathVariable("id") Long id) {
         return vacancyService.getVacancyByCategory(id);
+    }
+
+
+    @GetMapping("responded/{vacancyId}")
+    public List<RespondedApplicantDto> findRespondedApplicantByVacancyId(@PathVariable("vacancyId") Long vacancyId) {
+        return vacancyService.getRespondedApplicantByVacancyId(vacancyId);
+    }
+
+    @GetMapping("active")
+    public List<VacancyDto> getActiveVacancies() {
+        return vacancyService.getAllActiveVacancies();
     }
 }
