ad6c2cc4a8d80ada217972ffb76952059a5a6dee
Sezima.n <sezimanurlanovna03@gmail.com>
2025-04-11 18:03:52 +0600

    created controller

---
+++ src/main/java/kg/attractor/job_search_project/controller/api/ResumeControllerApi.java
@@ -0,0 +1,55 @@
+package kg.attractor.job_search_project.controller.api;
+import jakarta.validation.Valid;
+import kg.attractor.job_search_project.dto.ResumeDto;
+import kg.attractor.job_search_project.service.ResumeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("resumes")
+@RequiredArgsConstructor
+public class ResumeControllerApi {
+    private final ResumeService resumeService;
+
+    @GetMapping
+    public List<ResumeDto> resumes() {
+        return resumeService.getAllResume();
+    }
+
+    @PostMapping("add")
+    public HttpStatus addResume(@RequestBody @Valid ResumeDto resumeDto){
+        resumeService.getCreateResume(resumeDto);
+        return HttpStatus.OK;
+    }
+
+    @PutMapping("update/{resumeId}")
+    public HttpStatus updateResume(@RequestBody @Valid ResumeDto resume, @PathVariable Long resumeId){
+        resumeService.getUpdateResume(resumeId, resume);
+        return HttpStatus.OK;
+    }
+
+    @DeleteMapping("delete/{resumeId}")
+    public HttpStatus  deleteResume(@PathVariable("resumeId") Long resumeId){
+        resumeService.getDeleteResume(resumeId);
+        return HttpStatus.OK;
+    }
+
+    @GetMapping("/category/{category}")
+    public List<ResumeDto> getAllResumeByCategory(@PathVariable String category) {
+        return resumeService.getAllResumeByCategory(category);
+    }
+
+    @GetMapping("find/{resumeId}")
+    public ResumeDto findResumeById(@PathVariable Long resumeId){
+        return resumeService.getFindResumeById(resumeId);
+    }
+
+
+    @GetMapping("applicant/{id}")
+    private List<ResumeDto> allResumesByApplicantID(@PathVariable Long id) {
+        return resumeService.getAllResumeByApplicantId(id);
+    }
+}
