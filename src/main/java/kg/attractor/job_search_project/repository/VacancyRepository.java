b50f17253a4749a1ff4f9a5612fefeaeeee082cd
Sezima.n <sezimanurlanovna03@gmail.com>
2025-04-22 17:48:07 +0600

    sort vacancy by created date and response

--- src/main/java/kg/attractor/job_search_project/repository/VacancyRepository.java
+++ src/main/java/kg/attractor/job_search_project/repository/VacancyRepository.java
@@ -1,6 +1,7 @@
 package kg.attractor.job_search_project.repository;
 import kg.attractor.job_search_project.model.Vacancy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +14,15 @@ public interface VacancyRepository extends JpaRepository<Vacancy, Long> {
 
     boolean existsCategoryById(Long category_id);
     List<Vacancy> findAllVacancyByIsActiveTrue();
+
+    List<Vacancy> findAllByOrderByCreatedDateDesc();
+
+
+    @Query("""
+            select v from Vacancy v 
+            left join v.response r 
+            GROUP BY v.id 
+            order by count(r.id) desc
+            """)
+    List<Vacancy> findAllOrderByResponseCount();
 }
