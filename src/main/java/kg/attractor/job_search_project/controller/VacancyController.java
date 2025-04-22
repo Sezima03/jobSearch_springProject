ed3643c5c4223fbcabf8e405cb7a346a313d17b5
Sezima.n <sezimanurlanovna03@gmail.com>
2025-04-22 17:49:43 +0600

    added controller

--- src/main/java/kg/attractor/job_search_project/controller/VacancyController.java
+++ src/main/java/kg/attractor/job_search_project/controller/VacancyController.java
@@ -1,7 +1,9 @@
 package kg.attractor.job_search_project.controller;
 
 import jakarta.validation.Valid;
+import kg.attractor.job_search_project.dto.ResumeDto;
 import kg.attractor.job_search_project.dto.VacancyDto;
+import kg.attractor.job_search_project.service.ResumeService;
 import kg.attractor.job_search_project.service.VacancyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -16,6 +18,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class VacancyController {
     private final VacancyService vacancyService;
+    private final ResumeService resumeService;
 
 
     @GetMapping("allVacancy")
@@ -23,6 +26,12 @@ public class VacancyController {
         List<VacancyDto> vacancyDtos = vacancyService.getVacancy();
         model.addAttribute("vacancies", vacancyDtos);
         return "list/allvacancy";
+    }
+    @GetMapping("allResume")
+    public String allResume(Model model){
+        List<ResumeDto> resumeDtos =resumeService.getAllResume();
+        model.addAttribute("resumes", resumeDtos);
+        return "list/allresume";
     }
 
     @GetMapping("info/{id}")
@@ -74,6 +83,13 @@ public class VacancyController {
         }
         vacancyService.getUpdateVacancy(vacancyDto, vacancyId);
         return "redirect:/";
+    }
+
+    @GetMapping("responded")
+    public String vacancyRespond(Model model) {
+       List<VacancyDto> vacancyDto = vacancyService.getVacancyByResponses();
+       model.addAttribute("vacancies", vacancyDto);
+        return "list/responseCountToVacancy";
     }
 
 
