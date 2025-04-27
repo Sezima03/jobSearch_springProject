c18d96392af24c730a684032e616c88e85c8f7e4
Sezima.n <sezimanurlanovna03@gmail.com>
2025-04-27 22:02:32 +0600

    Added controller for password recovery via email

--- src/main/java/kg/attractor/job_search_project/controller/AuthController.java
+++ src/main/java/kg/attractor/job_search_project/controller/AuthController.java
@@ -1,16 +1,76 @@
 package kg.attractor.job_search_project.controller;
+import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpServletRequest;
+import kg.attractor.job_search_project.exceptions.UserNotFoundException;
+import kg.attractor.job_search_project.model.User;
+import kg.attractor.job_search_project.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.UnsupportedEncodingException;
 
 @Controller
 @RequestMapping("auth")
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final UserService userService;
+
     @GetMapping("login")
     public String login() {
         return "temp/login";
     }
+
+    @GetMapping("forgot_password")
+    public String showForgotPassword() {
+
+        return "passTemp/forgot_password_form";
+    }
+
+    @PostMapping("forgot_password")
+    public String processForgotPassword(HttpServletRequest request, Model model) {
+        try {
+            userService.makeResetPasswdLnk(request);
+            model.addAttribute("message", "Password reset link has been sent");
+        }catch (UserNotFoundException | UnsupportedEncodingException e){
+            model.addAttribute("error", e.getMessage());
+        } catch (MessagingException e) {
+            model.addAttribute("error", "error while sending email");
+        }
+
+        return "passTemp/forgot_password_form";
+    }
+
+    @GetMapping("/reset_password")
+    public String showResetPasswordForm(@RequestParam String token, Model model) {
+        try {
+            userService.getByResetPasswordToken(token);
+            model.addAttribute("token", token);
+        }catch (UserNotFoundException e){
+            model.addAttribute("error", "invalid token");
+        }
+
+        return "passTemp/reset_password_form";
+    }
+
+    @PostMapping("/reset_password")
+    public String processResetPassword(HttpServletRequest request, Model model) {
+        String token = request.getParameter("token");
+        String password = request.getParameter("password");
+        try {
+            User user = userService.getByResetPasswordToken(token);
+            userService.updatePassword(user, password);
+            model.addAttribute("message", "You have successfully changed your password.");
+        } catch (UsernameNotFoundException ex) {
+            model.addAttribute("message", "Invalid Token");
+        }
+        return "passTemp/passMessage";
+    }
+
 }
