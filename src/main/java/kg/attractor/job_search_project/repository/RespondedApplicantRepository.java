fcf57183e997a0bd5c17aed968547d7b6b1b1d6f
Sezima.n <sezimanurlanovna03@gmail.com>
2025-05-27 06:15:29 +0600

    add vacancy filter by position and minimum salary

--- src/main/java/kg/attractor/job_search_project/repository/RespondedApplicantRepository.java
+++ src/main/java/kg/attractor/job_search_project/repository/RespondedApplicantRepository.java
@@ -5,15 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface RespondedApplicantRepository extends JpaRepository<RespondedApplicant, Long> {
     List<RespondedApplicant> findAllByVacancyId(Long vacancyId);
     int countRespondedApplicantByUserId(Long userId);
-    int countRespondedApplicantByVacancyIdIn(List<Long> vacancyIds);
     List<RespondedApplicant> findAllRespondedApplicantByUserId(Long userId);
 
-    List<RespondedApplicant> findByVacancyIdIn(List<Long> vacancyId);
-    Optional<RespondedApplicant> findResumeByUserId(Long userId);
 }
